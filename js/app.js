5b795d041f2b5e3a1840244f37025faa46435cfc
Primemerlinian <mwatson0518@gmail.com>
2022-12-09 12:53:17 -0500

    exercise 4 complete

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-let board, turn, winner, tie,
+let board, turn, winner, tie
 
 /*------------------------ Cached Element References ------------------------*/
 
@@ -30,4 +30,35 @@ const messageEl = document.querySelector('#message')
     render()
   }
 
-  
+  function render() {
+    updateBoard ()
+    updateMessage ()
+  }
+
+  function updateBoard() {
+    board.forEach((space,index)=> {
+      const choice = squareEls[index]
+      if (space === -1)
+      return choice.textContent = 'O'
+      if (space == 1)
+      return choice.textContent = 'X'
+    });
+  }
+
+  function updateMessage() {
+    if (turn === 1 && winner === null) {
+      return messageEl.textContent = 'Player X turn!'
+    }
+    if (turn === -1 && winner === null) {
+      return messageEl.textContent = 'Player O turn!'
+    }
+    if (winner === 'false') {
+      return messageEl.textContent = "It's a Tie!"
+    }
+    if (winner === 1) {
+      return messageEl.textContent = 'Congratulations X won!'
+    }
+    if (winner === -1) {
+      return messageEl.textContent = 'Congratulations O won!'
+    }
+  }
