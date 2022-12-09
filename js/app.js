0429439d8e0a14f515287c5c450f71a77d4b3dc1
Primemerlinian <mwatson0518@gmail.com>
2022-12-09 12:22:51 -0500

    step 3 complete

--- js/app.js
+++ js/app.js
@@ -4,13 +4,13 @@
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-let board, turn, winner
+let board, turn, winner, tie,
 
 /*------------------------ Cached Element References ------------------------*/
 
 const squareEls = document.querySelectorAll('.squares')
 
-const messagleEl = document.querySelector('#message')
+const messageEl = document.querySelector('#message')
 
 
 
@@ -21,3 +21,13 @@ const messagleEl = document.querySelector('#message')
 
 /*-------------------------------- Functions --------------------------------*/
 
+  init()
+  function init () {
+    board = [null, null, null, null, null, null, null, null, null]
+    turn = 1
+    winner = false
+    tie = false
+    render()
+  }
+
+  
